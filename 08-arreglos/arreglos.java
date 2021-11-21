c3db7b40270a49b31259d47f39cf75b86121b282
alfredo12 <a.castillo.1r@edu.utc.mx>
2021-11-21 14:07:02 -0600

    tarea

--- 08-arreglos/arreglos.java
+++ 08-arreglos/arreglos.java
@@ -10,10 +10,12 @@ public class arreglos {
 
     public void menu(){
         System.out.println("elija una opcion deseada: ");
-        System.out.println("a.");
-        System.out.println("b.- ");
-        System.out.println("c.- ");
-        System.out.println("d.- ");
+        System.out.println("a. Elabore un programa que lea por el teclado 10 números enteros y los guarde en un Array");
+        System.out.println("b.- Elabore un programa que lea 10 números enteros por teclado y los guarde en un array. ");
+        System.out.println("c.- Realice un programa que permita leer 10 calificaciones de una asignatura y los guarde en un array para que muestre: ");
+        System.out.println("d.- matiz 3x3 ");
+        System.out.println("e.- matiz 4x4 ");
+        System.out.println("f, - palíndronma");
         System.out.println("Cualquier otra opcion sale del programa");
 
         op = entrada.next().charAt(0);
@@ -28,14 +30,28 @@ public class arreglos {
                 arreglos2();
                 break;
 
-            case 'c':
-            arreglos3();
-            break;
+             case 'c':
+             arreglos3();
+             break;
+
+             case 'd':
+             arreglos4();
+             break;
+
+             case 'e':
+             arreglos5();
+             break;
+
+             case 'f':
+             arreglos6();
+             break;
+
+
 
             
 
-            default: 
-                System.out.println("Gracias por jugar");
+             default: 
+                System.out.println("Gracias por su atencion ");
         }
     
     }
@@ -49,11 +65,10 @@ public class arreglos {
     
             Scanner sc = new Scanner(System.in);
             int i;
-            int pos = 0, neg = 0; //contadores de los números positivos y negativos
-            int[] numeros = new int[10]; //array que contendrá los números leídos por teclado
-            double sumaPos = 0, sumaNeg = 0; //acumuladores para las sumas de positivos y negativos                   
-            
-            //lectura de datos y llenar el array
+            int pos = 0, neg = 0;
+            int[] numeros = new int[10]; 
+            double sumaPos = 0, sumaNeg = 0; 
+           
             System.out.println("Lectura de los elementos del array: ");
             for (i = 0; i < 10; i++) {
                 System.out.print("numeros[" + i + "]= ");
@@ -63,10 +78,10 @@ public class arreglos {
             //recorrer el array para sumar por separado los números positivos 
              // y los negativos 
             for (i = 0; i < 10; i++) {
-                if (numeros[i] > 0){ //sumar positivos
+                if (numeros[i] > 0){
                     sumaPos += numeros[i];
                     pos++;
-                } else if (numeros[i] < 0){ //sumar negativos
+                } else if (numeros[i] < 0){ 
                     sumaNeg += numeros[i];
                     neg++;
                 }
@@ -95,17 +110,17 @@ public class arreglos {
                 int[] numeros = new int[10];
                 double media = 0;
 
-        //lectura de datos y llenar el array
+        
                 System.out.println("Lectura de los elementos del array: ");
                 for (i = 0; i < 10; i++) {
                 System.out.print("numeros[" + i + "]= ");
                 numeros[i] = sc.nextInt();
                 }
         
-        //Recorrer el array y calcular la media
+        
                  for (i = 0; i < 10; i++) {
-                 if (i % 2 == 0){ //si la posición actual es par
-                 media = media + numeros[i]; //se suma el valor de esa posición
+                 if (i % 2 == 0){ 
+                 media = media + numeros[i]; 
                  }
            
              }
@@ -119,34 +134,33 @@ public class arreglos {
         int numAlum, i;
         double suma = 0, media;
 
-        //Lectura del número de alumnos. Debe ser un valor positivo
+        
         do {
             System.out.print("Número de alumnos de la clase: ");
             numAlum = sc.nextInt();
         } while (numAlum <= 0);
 
-        //se crea un array llamado notas de numAlumn elementos de tipo double
+        
         double[] notas = new double[numAlum]; 
         
-        // Entrada de datos. Se lee la nota de cada alummo y se guarda
-        // en cada elemento del array
+        
         for (i = 0; i < notas.length; i++) {
             System.out.print("Alumno " + (i + 1) + " Nota final: ");
             notas[i] = sc.nextDouble();
         }
 
-        // Sumar todas las notas
+        
         for (i = 0; i < notas.length; i++) {
             suma = suma + notas[i];
         }
 
-        // Calcular la media
+        
         media = suma / notas.length;
 
-        // Mostrar la media
+        
         System.out.printf("Nota media del curso: %.2f %n", media);
 
-        // Mostrar los valores superiores a la media
+        
         System.out.println("Listado de notas superiores a la media: ");
         for (i = 0; i < notas.length; i++) {
             if (notas[i] > media) {
@@ -154,6 +168,85 @@ public class arreglos {
             }
         }
     }
-}
+
+
+
+     private void arreglos4(){
+        
+        int i;
+int j;
+int matriza[][]=new int[3][3];
+
+Scanner dato = new Scanner (System.in);
+System.out.println("Escribir datos de la matriz ");
+
+    for (i=0;i<=2;i++){
+        for (j=0;j<=2;j++){
+            System.out.print("Escribir valor " + i + " , " + j + " : ");
+            matriza [i][j] = dato.nextInt();
+        }
+    }
+         System.out.println("MATRIZ 3x3");
+        for (i=0;i<=2;i++){
+         for (j=0;j<=2;j++){
+         System.out.print(matriza[i][j]+"");
+        }
+         System.out.println("");
+         }
+       } 
+
+
+        private void arreglos5(){
+            int i;
+         int j;
+          int matriza[][]=new int[4][4];
+
+          Scanner dato = new Scanner (System.in);
+         System.out.println("Escribir datos de la matriz ");
+
+         for (i=0;i<=3;i++){
+          for (j=0;j<=3;j++){
+            System.out.print("Escribir valor " + i + " , " + j + " : ");
+            matriza [i][j] = dato.nextInt();
+         } 
+         }
+          System.out.println("MATRIZ 4x4");
+          for (i=0;i<=3;i++){
+          for (j=0;j<=3;j++){
+          System.out.print(matriza[i][j]+"");
+        }
+         System.out.println("");
+         }
+         }
+         private void arreglos6(){
+            Scanner sc= new Scanner(System.in);
+            System.out.print("Introduce una frase (puede tener puntos, comas y espacios): ");
+            String s=sc.nextLine();
+            
+            s=s.replace(" ", "");
+            s=s.replace(",", "");
+            s=s.replace(".", "");
+            System.out.print(s);
+            int fin = s.length()-1;
+            int ini=0;
+            boolean espalin=true;
+            
+            while(ini < fin){
+                if(s.charAt(ini)!=s.charAt(fin)){
+                    espalin=false;
+                }
+            ini++;
+            fin--;
+            }
+            if(espalin)
+                System.out.print("\nEs palindromo.");
+            else
+                System.out.print("\nNo es palindromo.");
+            
+        }
+    }
+         
+
+       
             
  
